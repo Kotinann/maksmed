bf8b6314229d54c08c0f0bf2e2e5ccb03a08f5ee
Kotinann <Kotina1601@gmail.com>
2023-02-28 21:43:43 +0400

    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,8 @@
 import styles from './footer.module.scss'
 
 export default function Footer() {
-    return (<div className={styles.footer}>
+    return (
+        <div className={styles.footer}>
 
         </div>
     )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,55 +1,43 @@
 import styles from './header.module.scss'
+import LogoIcon from "../../assets/icons/logo.svg"
+import PlaceIcon from "../../assets/icons/place-localizer.svg"
+import ClockIcon from "../../assets/icons/clock.svg"
 
 export default function Header() {
-    return (<div className={styles.header}>
-                <div className="row no-gutters">
-                    <div className="col-7 col-xl-4">
-                        <div className="logo">
-                            <a href='#'>
-                                <img src="public/img/icon/logo.svg" alt="logo">
-                                    <div className="logo__sitename">
-                                        <div className="german-flag german-flag_vertical">
-
-                                        </div>
-                                    </div>
-                            </a>
-                        </div>
+    return (
+        <div className={styles.header}>
+            <div className="container">
+                <div className={styles.wrapper}>
+                    <div className={styles.logo}>
+                        <a className={styles.logoLink} href="#">
+                            <LogoIcon />
+                        </a>
                     </div>
-                    <div className="col-5 col-xl-8 header__right">
-                        <div className="contact-info d-none d-xl-flex">
-                            <div className="contact-info__item ml-2">
-                                <div className="contact-info__icon">
-                                    <img src="assets/img/icon/place-localizer.svg" alt="">
+                    <div className={styles.right}>
+                        <div className={styles.contactInfo}>
+                            <div className={styles.contactInfoItem}>
+                                <div className={styles.contactInfoIcon}>
+                                    <PlaceIcon />
                                 </div>
-                                <div className="contact-info__text">Пермь, <br> ул. Куйбышева, 71/1</div>
+                                <div className={styles.contactInfoText}>Самара,<br/>ул. Ново-Садовая улица, 252</div>
                             </div>
-                            <div className="contact-info__item justify-content-end ml-7">
-                                <div className="contact-info__icon">
-                                    <div className="icon icon-clock">
-                                        <img src="assets/img/icon/clock.svg" alt="">
-                                    </div>
+                            <div className={styles.contactInfoItem}>
+                                <div className={styles.contactInfoIcon}>
+                                        <ClockIcon />
                                 </div>
-                                <div className="contact-info__text">
-                                    <div className="contact-info__text-row">
-                                        <div className="contact-info__text-day">ПН-ПТ:</div>
-                                        <div className="contact-info__text-hour">09:00~21:00</div>
-                                    </div>
-                                    <div className="contact-info__text-row">
-                                        <div className="contact-info__text-day">СБ:</div>
-                                        <div className="contact-info__text-hour">10:00~15:00</div>
-                                    </div>
+                                <div className={styles.contactInfoText}>
+                                        <div className={styles.contactInfoDay}>ПН, СР, ПТ:</div>
+                                        <div className={styles.contactInfoHour}>09:00~20:00</div>
                                 </div>
                             </div>
                         </div>
-                        <div className="phone-block"><a className="phone-block__callback popup-form" href="#callback">Заказать
-                            звонок</a>
-                            <div className="phone-block__phone"><a href="tel:+73422079694"><span
-                                className="phone-block__phone-code">(342)</span><span
-                                className="phone-block__phone-number">207-96-94</span></a></div>
-                        </div>
+                        <div className={styles.phone}><span
+                            className={styles.phoneCode}>(927)</span><span
+                            className={styles.phoneNumber}>260-20-65</span></div>
                     </div>
                 </div>
-    </div>
-)
+            </div>
+        </div>
+    )
 
 }
