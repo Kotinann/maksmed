ca451cc5befd49813531a58eae3492ecef20a37a
andrewkotin <htc95@ya.ru>
2023-05-23 18:07:07 +0500

    пофиксил футер

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,49 +7,51 @@ export default function Footer() {
         <div className={styles.footer}>
             <div className="container">
                 <div className={styles.wrapper}>
-                    <div className={styles.menu}>
-                        <ul className={styles.list}>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    Главная
-                                </a>
-                            </li>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    О клинике
-                                </a>
-                            </li>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    Цены
-                                </a>
-                            </li>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    Отзывы
-                                </a>
-                            </li>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    Дипломы и сертификаты
-                                </a>
-                            </li>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    Документы
-                                </a>
-                            </li>
-                            <li className={styles.items}>
-                                <a href="#" className={styles.link}>
-                                    Политика конфиденциальности
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={styles.reverseCol}>
+                    <div className={styles.row}>
+                        <div className={styles.menu}>
+                            <ul className={styles.list}>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        Главная
+                                    </a>
+                                </li>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        О клинике
+                                    </a>
+                                </li>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        Цены
+                                    </a>
+                                </li>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        Отзывы
+                                    </a>
+                                </li>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        Дипломы и сертификаты
+                                    </a>
+                                </li>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        Документы
+                                    </a>
+                                </li>
+                                <li className={styles.items}>
+                                    <a href="#" className={styles.link}>
+                                        Политика конфиденциальности
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles.reverseCol}>
                             <div className={styles.license}>
                                 <p>ООО «Максмед».<br/>Пермь, Самара, ул.Ново-Садовая 252
-                                    <br/>Лицензия № ЛО-63-01-004694 от 4 июля 2018 г.</p>
+                                    <br/>Лицензия № ЛО-63-01-004694 от 4 июля 2018 г.
+                                </p>
                             </div>
                             <div className={styles.footerSocial}>
                                 <div className={styles.social}>
@@ -61,22 +63,23 @@ export default function Footer() {
                                     </a>
                                 </div>
                             </div>
+                        </div>
                     </div>
-                        <div className={styles.proticopokaz}>
+                    <div className={styles.row}>
+                        <div className={styles.contraindications}>
+                            Внимание! Имеются противопоказания. необходима консультация специалиста
+                        </div>
+                        <div className={styles.cookie}>
                             <p>
-                                Внимание! Имеются противопоказания. необходима консультация специалиста
+                                Этот сайт использует «cookies». Условия использования «cookies» см. на странице
+                                «Конфиденциальность персональной информации». Также сайт использует Интернет-сервис
+                                для
+                                сбора технических данных касательно посетителей с целью получения маркетинговой и
+                                статистической информации. Условия обработки данных посетителей сайта см. в <a
+                              href="/privacy.html">Политике конфиденциальности</a>. Нажимая кнопку «ОК», вы
+                                подтверждаете ваше согласие на обработку данных на указанных выше условиях.
                             </p>
                         </div>
-                    <div className={styles.cookie}>
-                        <p>
-                            Этот сайт использует «cookies». Условия использования «cookies» см. на странице
-                            «Конфиденциальность персональной информации». Также сайт использует Интернет-сервис
-                            для
-                            сбора технических данных касательно посетителей с целью получения маркетинговой и
-                            статистической информации. Условия обработки данных посетителей сайта см. в <a
-                            href="/privacy.html">Политике конфиденциальности</a>. Нажимая кнопку «ОК», вы
-                            подтверждаете ваше согласие на обработку данных на указанных выше условиях.
-                        </p>
                     </div>
                 </div>
             </div>
